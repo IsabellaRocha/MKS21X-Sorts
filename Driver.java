7eb22546f16644e788decf1df1291afc44d71288
IsabellaRocha <irocha00@stuy.edu>
2018-12-07 11:36:13 -0500

    Added more test cases for bubbleSort

--- Driver.java
+++ Driver.java
@@ -13,11 +13,17 @@ public class Driver {
   public static void main(String[] args) {
     int[] ary = {1,7,43,7,56,3,8,25345,8};
     int[] ary2 = {234762837, 23454, 345, 17, 853533535, -123};
+    int[] ary3 = {1,7,43,7,56,3,8,25345,8};
+    int[] ary4 = {234762837, 23454, 345, 17, 853533535, -123};
     System.out.println(printArray(ary));
-    Sorts.selectionsort(ary);
+    Sorts.selectionSort(ary);
     System.out.println(printArray(ary));
     System.out.println(printArray(ary2));
-    Sorts.selectionsort(ary2);
+    Sorts.selectionSort(ary2);
     System.out.println(printArray(ary2));
+    System.out.println(printArray(ary3));
+    Sorts.bubbleSort(ary3);
+    System.out.println(printArray(ary4));
+    Sorts.bubbleSort(ary4);
   }
 }
